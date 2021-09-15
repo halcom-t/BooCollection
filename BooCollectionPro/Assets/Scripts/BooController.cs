a08731694874b9a2d90717678c263762ba538414
halcom <okazawa.game@gmail.com>
2021-09-15 18:20:34 +0900

    背景と皿画像追加＆画像の最大サイズの変更

---
+++ BooCollectionPro/Assets/Scripts/BooController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ÉuÅ[ÇÃäÓñ{ìÆçÏ
+/// </summary>
+public class BooController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- BooCollectionPro/Assets/Scripts/GameManager.cs
+++ BooCollectionPro/Assets/Scripts/GameManager.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// ƒQ[ƒ€‘S‘Ì‚ÌŠÇ—
+/// </summary>
 public class GameManager : MonoBehaviour
 {
     /// <summary>
